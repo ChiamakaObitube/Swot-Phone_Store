f277f4398021d34c9b37bdd57f5ce903a2f386cc
chiamakaobitube <obitubechiamaka@gmail.com>
2019-09-12 21:34:16 +0100

    commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -48,10 +48,11 @@ export default class Details extends Component {
                                     </ButtonContainer>
                                 </Link>
                                 <ButtonContainer
-                                
+
                                 disabled={inCart ? true : false }
                                 onClick={() => {
                                     value.addToCart(id);
+                                    value.openModal(id);
                                 }}>
                                     {inCart ? 'inCart' : 'add to cart' }
                                 </ButtonContainer>
--- src/components/Product.js
+++ src/components/Product.js
@@ -10,7 +10,11 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    <div className="img-container p-5" onClick={() => console.log('You clicked me on the image container')}>
+                    <ProductConsumer>
+                        {value => (
+                    <div className="img-container p-5" 
+                    onClick={() =>  value.handleDetail(id)
+                     }>
                         
                         <Link to="/details">
                             <img src={img} alt="product" className="card-img-top"/> 
@@ -18,7 +22,8 @@ export default class Product extends Component {
                         <button className="cart-btn" 
                         disabled={inCart ? true: false} 
                         onClick={() => { 
-                            console.log('added to the cart');
+                            value.addToCart(id);
+                            value.openModal(id);
                         }}
                         >
                         {inCart ? (
@@ -31,7 +36,8 @@ export default class Product extends Component {
                             )}
                         </button>
                     </div>
-
+                        )}
+                    </ProductConsumer>
                     {/* card footer */}
                     <div className="card-footer d-flex justify-content-between">
                     <p className="align-self-center mb-0">
