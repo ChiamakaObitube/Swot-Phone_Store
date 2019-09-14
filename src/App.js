6af5240643c0724a943cfb6b2089b3154ed2cd86
chiamakaobitube <obitubechiamaka@gmail.com>
2019-09-14 22:28:27 +0100

    modal component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 
 import ProductList from './components/ProductList';
-import Cart from './components/Cart';
+import Cart from './components/Cart/Cart';
 import Default from './components/Default';
 import Details from './components/Details';
 import Modal from './components/Modal';
